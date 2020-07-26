6bf9a11235f1ed5e54312d8f84158de4da09396a
Laveesh Gupta <loveeshgoyal@gmail.com>
2020-07-26 10:21:30 +0530

    Navigation bar and  home and about views added

---
+++ src/Views/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1 className='font-bold text-2xl'>This is About page</h1>
+      <p>This is about page content</p>
+    </div>
+  );
+}
+
+export default About;
